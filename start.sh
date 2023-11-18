ce2abd90d215fe07bda0ffc676b2ae0461c9e7dc
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-11-18 16:51:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/afluaflu123/Hacker-bro.git /Hacker-bro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Hacker-bro
 fi
-cd /Auto-search-tamil-bot
+cd /Hacker-bro
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Fuck You Bot...."
 python3 bot.py
