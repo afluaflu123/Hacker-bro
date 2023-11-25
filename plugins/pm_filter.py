a8d3de53b2a8f5ec41ef8c8f7613120e9a4864ee
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-11-25 19:40:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1214,7 +1214,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.SOURCE_TXT,
-            replymarkup=reply_markup,
+            reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
     elif query.data == "manuelfilter":
