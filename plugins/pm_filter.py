613fcb724ae25e637f61c9c27a00c21aa660ad2a
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-11-26 01:51:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -55,8 +55,6 @@ async def give_filter(client, message):
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
-    files, n_offset, total = await get_search_results(message.text, filter=True)
-    if int(total) != 0:
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
