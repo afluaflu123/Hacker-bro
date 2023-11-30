97ef1fd4f5606154993c753f8f36e4c59dc6a964
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-11-30 15:22:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1275,7 +1275,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         cpu_usage = psutil.cpu_percent()
         ram_usage = psutil.virtual_memory().percent
         disk_usage = psutil.disk_usage('/').percent
-        await query.answer(f"⚡️ Sʏsᴛᴇᴍ Sᴛᴀᴛᴜs ⚡️\n\n❂ Uᴘᴛɪᴍᴇ : {currentTime}\n✇ Cᴘᴜ : {cpu_usage}\n✪ Rᴀᴍ : {ram_usage}\n✼ Tᴏᴛᴀʟ Dɪsᴋ : {total}\n❐ Usᴇᴅ Sᴘᴀᴄᴇ : {used} ({disk_usage}%)\n❦ Fʀᴇᴇ Sᴘᴀᴄᴇ : {free}\n\nᴠ2.9.1 [sᴛᴀʙʟᴇ]", show_alert=True)    
+        await query.answer(f"⚡️ sᴇʀᴠᴇʀ sᴛᴀᴛᴜs ⚡️\n\n◉ ᴜᴘᴛɪᴍᴇ : {currentTime}\n◉ ᴄᴘᴜ : {cpu_usage}\n◉ ʀᴀᴍ : {ram_usage}\n◉ ᴛᴏᴛᴀʟ ᴅɪsᴋ : {total}\n◉ ᴜsᴇᴅ sᴘᴀᴄᴇ : {used} ({disk_usage}%)\n◉ ғʀᴇᴇ sᴘᴀᴄᴇ : {free}", show_alert=True)    
 
     elif query.data == "start":
         buttons = [[
